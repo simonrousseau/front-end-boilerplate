3d397887ef29bc24e7cd775a2020688cbd79100e
Simon <simonrousseau22@gmail.com>
2021-10-26 23:05:16 +0200

    Feat: setup project routing

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,13 +1,44 @@
 import React from 'react';
-import {Layout} from './Layout'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import Project from './Project';
+import Roadmap from './Roadmap';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Layout/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/project">Project</Link>
+                            </li>
+                            <li>
+                                <Link to="/roadmap">Roadmap</Link>
+                            </li>
+                        </ul>
+                    </nav>
+
+                    <Switch>
+                        <Route path="/project">
+                            <Project />
+                        </Route>
+                        <Route path="/roadmap">
+                            <Roadmap />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
